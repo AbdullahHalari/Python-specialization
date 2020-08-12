7773383f9ac4c48e4e757cfa95fb04236436827c
Abdullah Halari <droidabdullah@gmail.com>
2020-08-12 10:29:01 -0700

    list

--- assigment.py
+++ assigment.py
@@ -174,14 +174,67 @@
 
 # Use the file name mbox-short.txt as the file name
 
+# fname = input("Enter file name: ")
+# fh = open(fname)
+# count = 0
+# average = 0
+# for line in fh:
+#     if not line.startswith("X-DSPAM-Confidence:") : continue
+#     average += float(line[20:-1].strip())
+#     count = count + 1
+#     #print(line)
+    
+# print("Average spam confidence:", (average/count))
+
+
+#list
+# .append()  for adding in list
+# .sort()    for arrange in alphabetic order
+# .split()   for convert string into list
+
+
+
+# a = ['abd', 5, 'c']
+# print(a[0])
+
+# x = list()
+# dir(x)
+
+# fruit = 'Banana'
+# fruit[0] = 'b'
+# print(fruit)
+
 fname = input("Enter file name: ")
+
 fh = open(fname)
 count = 0
-average = 0
+for i in fh:
+    fx = i.strip()
+    fx = i.split()
+    if i.startswith('From:'):
+        print(fx[1])
+        count = count + 1   
+      
+
+print("There were", count, "lines in the file with From as the first word")
+
+fname = input("Enter file name: ")
+fh = open(fname)
+
+lst = list()
+
 for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:") : continue
-    average += float(line[20:-1].strip())
-    count = count + 1
-    #print(line)
     
-print("Average spam confidence:", (average/count))
+    x = line.rstrip()
+    y = x.split()
+    for i in y:
+       if i in lst:
+            continue
+            
+       else:
+                
+          lst.append(i) 
+    
+    
+lst.sort()    
+print(lst)
